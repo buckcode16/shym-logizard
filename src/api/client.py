f609f203cb56aa298f80204fe73d20926b907a81
Bucky <buckcode16@gmail.com>
2025-12-26 10:48:12 +0900

    feat(httpx client): add timeout to client

--- src/api/client.py
+++ src/api/client.py
@@ -4,7 +4,10 @@ from typing import Type, TypeVar
 import httpx
 from pydantic import BaseModel
 
+# bounded generic type variable to enforce response_model to be either BaseModel
+# or BaseModel inheritance
 T = TypeVar("T", bound=BaseModel)
+timeout = httpx.Timeout(10.0, read=120.0)
 
 
 class LogizardClient:
@@ -24,14 +27,23 @@ class LogizardClient:
         # )
 
         # self.client = httpx.AsyncClient(timeout=timeout_config, limits=limits_config)
-        self.client = httpx.AsyncClient()
+        self.client = httpx.AsyncClient(timeout=timeout)
 
+        # !revisit why return self not client
         return self
 
+    # !revisit learn exc_type, exc_val, exc_tb
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         if self.client:
+            # !revisit why self.client close, how about if other variables instantiated
             await self.client.aclose()
 
+    # response_model expect class definitions (Type[T]) instead of instance (T)
+    # response from client.post returns raw byte but implicitly known as json
+    # response.json() converst to python dict
+    # pydantic model does not accept dict since it's a class, therefore unpack operator
+    # (**) is used to convert dict to keyword argumenets which can then be used to
+    # instantiate a pydantic class.
     async def post_json(
         self,
         url: str,
